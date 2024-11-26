f55b36e2438ae048bf36d93876b9578b01da1074
Nocturne2282282 <206088020@qq.com>
2024-11-26 19:29:25 +0800

    f22hw01: all problems solved

--- f22hw01/testski.sh
+++ f22hw01/testski.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 echo "python3 ./src/main_ski.py ./test/test.ski"
 python3 ./src/main_ski.py ./test/test.ski
+echo "python3 ./src/main_ski.py ./problem.ski"
+python3 ./src/main_ski.py ./problem.ski
+echo "python3 test/test_numpy.py"
+python3 test/test_numpy.py
